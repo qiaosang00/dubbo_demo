93dadcb314e72a20c9ba2d6ee5c62c986f087822
zhonghuarong <2670625013@qq.com>
2019-06-27 17:39:40 +0800

    Initial commit

---
+++ dubbo_provider/src/main/java/com.provider.service/DemoApiImpl.java
@@ -0,0 +1,10 @@
+package com.provider.service;
+
+import com.api.service.DemoApi;
+
+public class DemoApiImpl implements DemoApi {
+@Override
+public String sayHello() {
+    return "Hello World!";
+}
+}
